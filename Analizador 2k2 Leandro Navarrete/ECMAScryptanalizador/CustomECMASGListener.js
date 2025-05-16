1611410c7d3c15760d979560951a2cc2a0b358aa
LNavarrete <leaneitor2005@gmail.com>
2025-05-16 14:29:01 -0300

    Delete Analizador 2k2 Leandro Navarrete directory

--- Analizador 2k2 Leandro Navarrete/ECMAScryptanalizador/CustomECMASGListener.js
+++
@@ -1,9 +0,0 @@
-import ECMASGListener from "./generated/ECMASGListener.js";
-
-export class ECMASGListener extends ECMASGListener {
-
-    enterStat(ctx) {
-        console.log(`Se detectó una: ${ctx.constructor.name}`);
-    }
-
-}
